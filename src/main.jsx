5e1c98373db86ea5484e7531e1c78577f76060f7
AnilBASARAN <nlbsrn@gmail.com>
2024-11-13 22:59:54 +0300

    adding stuff to main.jsx redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './routers/router.jsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-     <RouterProvider router={router} />
-  </StrictMode>,
+  <Provider store={store} >
+     <RouterProvider  router={router} />
+  </Provider>,
 )
