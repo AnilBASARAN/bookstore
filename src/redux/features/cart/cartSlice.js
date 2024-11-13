7c0f58838a6df91d6ab7a75aa2cd48999d73b195
AnilBASARAN <nlbsrn@gmail.com>
2024-11-14 01:55:17 +0300

    ading some sweet aditions

--- src/redux/features/cart/cartSlice.js
+++ src/redux/features/cart/cartSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import  Swal  from 'sweetalert2';
 
 const initialState = {
   cartItems: [],
@@ -12,9 +13,20 @@ const cartSlice = createSlice({
     const existingItem = state.cartItems.find(item=> item._id === action.payload._id);
     if(!existingItem){
         state.cartItems.push(action.payload)
-        alert("Item added successfully!")
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Product added to the cart",
+            showConfirmButton: false,
+            timer: 1500
+          });
     }else{
-        alert("Item already exists")
+        Swal.fire({
+            icon: "error",
+            title: "This Item already exists in your cart"
+            
+          });
+        
     }
    } 
   }
