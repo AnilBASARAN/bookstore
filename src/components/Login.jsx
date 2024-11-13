b1372e58d204547528dbfd3edc552c0ede432b3e
AnilBASARAN <nlbsrn@gmail.com>
2024-11-13 16:59:54 +0300

    creating register and login pages

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,26 +1,39 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { FaGoogle } from "react-icons/fa";
+import { useForm } from "react-hook-form"
 
 
 
 const Login = () => {
     const [message, setMessage] = useState("")
+    const [submitData,setSubmitData] = useState({});
+        const {
+          register,
+          handleSubmit,
+          watch,
+          formState: { errors },
+        } = useForm()
 
     const navigate = useNavigate()
    
+    const onSubmit = (data) => setSubmitData(data);
+        console.log(submitData)
 
+    const handleGoogleSignIn=()=>{
+
+    }
   
   return (
     <div className='h-[calc(100vh-120px)] flex justify-center items-center '>
         <div className='w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
             <h2 className='text-xl font-semibold mb-4'>Please Login</h2>
 
-            <form >
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className='mb-4'>
                     <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="email">Email</label>
                     <input 
-                
+                    {...register("email", { required: true })}
                     type="email" name="email" id="email" placeholder='Email Address'
                     className='shadow text-lg appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow'
                     />
@@ -28,7 +41,7 @@ const Login = () => {
                 <div className='mb-4'>
                     <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="password">Password</label>
                     <input 
-                   
+                   {...register("password", { required: true })}
                     type="password" name="password" id="password" placeholder='Password'
                     className='shadow text-lg appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow'
                     />
@@ -37,17 +50,17 @@ const Login = () => {
                     message && <p className='text-red-500 text-xs italic mb-3'>{message}</p>
                 }
                 <div>
-                    <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none'>Login </button>
+                    <button className='bg-blue-500 text-sm hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none'>Login </button>
                 </div>
             </form>
-            <p className='align-baseline font-medium mt-4 text-sm'>Haven't an account? Please <Link to="/register" className='text-blue-500 hover:text-blue-700'>Register</Link></p>
+            <p className='align-baseline font-medium mt-4 text-sm'>Haven&apos;t an account? Please <Link to="/register" className='text-blue-500 hover:text-blue-700'>Register</Link></p>
 
             {/* google sign in */}
-            <div className='mt-4'>
+            <div className='mt-4 w-full '>
                 <button 
-        
-                className=' flex flex-wrap gap-1 items-center justify-center bg-blue-800 hover:bg-blue-600  text-white font-bold py-2 px-4 rounded focus:outline-none'>
-                <FaGoogle  className='mr-2'/>
+                onClick={handleGoogleSignIn}
+                className=' flex text-sm h-[40px] w-full flex-wrap gap-1 items-center justify-center bg-blue-800 hover:bg-blue-600  text-white font-bold py-2 px-4 rounded focus:outline-none'>
+                <FaGoogle  className='mr-2 text-red-500 text-2xl'/>
                 Sign in with Google
                 </button>
             </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { FaGoogle } from "react-icons/fa";
+import { useForm } from "react-hook-form"
 
 const Register = () => {
+  const [message, setMessage] = useState("")
+  const [submitData,setSubmitData] = useState({});
+      const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+      } = useForm()
+
+  const navigate = useNavigate()
+ 
+  const onSubmit = (data) => setSubmitData(data);
+      console.log(submitData)
+
+  const handleGoogleSignIn=()=>{
+
+  }
+
   return (
-    <div>Register</div>
+    <div>
+        <div className='h-[calc(100vh-120px)] flex justify-center items-center '>
+        <div className='w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+            <h2 className='text-xl font-semibold mb-4'>Please Register</h2>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className='mb-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="email">Email</label>
+                    <input 
+                    {...register("email", { required: true })}
+                    type="email" name="email" id="email" placeholder='Email Address'
+                    className='shadow text-lg appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow'
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label className='block text-gray-700 text-sm font-bold mb-2' htmlFor="password">Password</label>
+                    <input 
+                   {...register("password", { required: true })}
+                    type="password" name="password" id="password" placeholder='Password'
+                    className='shadow text-lg appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow'
+                    />
+                </div>
+                {
+                    message && <p className='text-red-500 text-xs italic mb-3'>{message}</p>
+                }
+                <div>
+                    <button className='bg-blue-500 text-sm hover:bg-blue-700 text-white font-bold py-2 px-8 rounded focus:outline-none'>Register </button>
+                </div>
+            </form>
+            <p className='align-baseline font-medium mt-4 text-sm'>You have an account? Please <Link to="/login" className='text-blue-500 hover:text-blue-700'>Login</Link></p>
+
+            {/* google sign in */}
+            <div className='mt-4 w-full '>
+                <button 
+                onClick={handleGoogleSignIn}
+                className=' flex text-sm h-[40px] w-full flex-wrap gap-1 items-center justify-center bg-blue-800 hover:bg-blue-600  text-white font-bold py-2 px-4 rounded focus:outline-none'>
+                <FaGoogle  className='mr-2 text-red-500 text-2xl'/>
+                Register with Google
+                </button>
+            </div>
+
+            <p className='mt-5 text-center text-gray-500 text-xs'>©2025 Book Store. All rights reserved.</p>
+        </div>
+    </div>
+    </div>
   )
 }
 
